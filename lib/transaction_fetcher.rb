19cb4bed217fe61ded627a4622b0da5df1a79f19
Kian Mayne <git@kianmayne.co.uk>
2017-06-10 17:19:11 +0100

    Fixed error if --since option is not supplied
    - Fixed default behaviour to request transactions from two weeks ago if
    no --since option is given (interpolating this default from the default
    in the method signature)
    - Changed help message to mention default of two weeks

--- lib/transaction_fetcher.rb
+++ lib/transaction_fetcher.rb
@@ -8,13 +8,16 @@ class TransactionFetcher
     @account_id = account_id
   end
 
-  def fetch(since: 2.weeks.ago)
+  def fetch(since: DEFAULT_DATE)
+    since = since || DEFAULT_DATE
     account_id = http_get('/accounts')['accounts'].first['id']
     transactions = http_get("/transactions?account_id=#{account_id}&since=#{since.strftime('%FT%TZ')}&expand[]=merchant")
     transactions['transactions'].map{|t| OmniStruct.new(t)}
   end
 
   private
+  TWO_WEEKS_SECONDS = 2 * 7 * 24 * 60 * 60
+  DEFAULT_DATE = Time.now - TWO_WEEKS_SECONDS
 
   def http_get(url)
     JSON.parse(RestClient.get("https://api.monzo.com#{url}", { 'Authorization' => "Bearer #{@access_token}" }))
