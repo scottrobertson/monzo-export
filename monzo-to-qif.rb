b1aebb791fdb05ae4f739a7177d36ef258ea76f6
Andrew Schofield <the.uncle.fungus@gmail.com>
2017-08-17 21:08:09 +0100

    Add support for current account

--- monzo-to-qif.rb
+++ monzo-to-qif.rb
@@ -18,13 +18,15 @@ command :generate do |c|
   c.option '--since TOKEN', String, 'The date (YYYY-MM-DD) to start exporting transactions from. Defaults to nil'
   c.option '--folder PATH', String, 'The folder to export to. Defaults to ./exports'
   c.option '--settled_only', String, 'Only export settled transactions'
+  c.option '--current_account', String, 'Export transactions from the current account instead of the prepaid account'
   c.action do |args, options|
     since = options.since ? Date.parse(options.since).to_time : nil
     if options.access_token
-      fetcher = TransactionFetcher.new(options.access_token, options.account_id)
+      fetcher = TransactionFetcher.new(options.access_token, current_account: options.current_account)
       transactions = fetcher.fetch(since: since)
       qif = QifCreator.new(transactions).create(options.folder, settled_only: options.settled_only)
 
+	  if options.current_account then say "Monzo Account: #{fetcher.account_and_sort}" end
       say "Balance: £#{fetcher.balance}"
     else
       say 'Please supply an access_token'
