98ca9b52ed80d00d363b998a31e7ceaa4f05e195
Scott Robertson <scottymeuk@gmail.com>
2017-08-18 20:04:39 +0100

    Bit of cleanup

--- monzo-to-qif.rb
+++ monzo-to-qif.rb
@@ -23,10 +23,9 @@ command :generate do |c|
     since = options.since ? Date.parse(options.since).to_time : nil
     if options.access_token
       fetcher = TransactionFetcher.new(options.access_token, current_account: options.current_account)
-      transactions = fetcher.fetch(since: since)
-      qif = QifCreator.new(transactions).create(options.folder, settled_only: options.settled_only)
+      qif = QifCreator.new(fetcher.fetch(since: since)).create(options.folder, settled_only: options.settled_only)
 
-	  if options.current_account then say "Monzo Account: #{fetcher.account_and_sort}" end
+      say "Monzo Account: #{fetcher.account_and_sort}" if options.current_account
       say "Balance: £#{fetcher.balance}"
     else
       say 'Please supply an access_token'
