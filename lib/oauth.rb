615f862930241b7915e6103668cdf9e4ff798f95
Andrew Schofield <the.uncle.fungus@gmail.com>
2017-10-04 21:43:24 +0100

    Improved OAuth workflow

--- lib/oauth.rb
+++ lib/oauth.rb
@@ -10,10 +10,16 @@ class OAuth
   
   def getAccessToken()
 	if !@config['access_token']
-	  puts 'OAuth not configured. Please run Monzo to QIF auth.'
+	  say 'OAuth not configured. Please run Monzo to QIF auth'
+	  abort
     end
 	if Time.now > @config['expiry']
-	  refreshToken(@config['clientId'], @config['clientSecret'], @config['refresh_token'])
+	  if @config['refresh_token']
+	    refreshToken(@config['clientId'], @config['clientSecret'], @config['refresh_token'])
+	  else
+	    say 'Existing token has expired, please re-authenticate.'
+	    initialAuth(@config['clientId'], @config['clientSecret'])
+	  end
 	end
 	@config['access_token']
   end
@@ -23,23 +29,26 @@ class OAuth
 	@config['clientSecret'] = clientSecret
 	@config['state'] = SecureRandom.urlsafe_base64(64)
 	saveConfig()
-    say "Open the following URL in a browser:"
+    say "Open the following URL in a browser and follow the instructions. When you recieve the response email, copy the url and pass it to the --authurl parameter"
     say "https://auth.getmondo.co.uk/?client_id=#{clientId}&redirect_uri=#{@redirectURI}&response_type=code&state=#{@config['state']}"
   end
   
   def processAuthUrl(authCode, state)
     if state != @config['state']
-	  say "Invalid callback state"
+	  say "Auth url state does not match internal state. Try setting up OAuth again."
+	  abort
 	 else
 	   exchangeAuthCode(@config['clientId'], @config['clientSecret'], @redirectURI, authCode)
-	   say "Access token retrieved"
 	 end
   end
   
   def exchangeAuthCode(clientId, clientSecret, redirectURI, authCode)
 	json = JSON.parse(RestClient.post("https://api.monzo.com/oauth2/token", {grant_type: 'authorization_code', client_id: @config['clientId'], client_secret: @config['clientSecret'], redirect_uri: @redirectURI, code: authCode}))
 	@config['access_token'] = json['access_token']
-	@config['refresh_token'] = json['refresh_token']
+	# non-confidential clients won't return a refresh token
+	if json['refresh_token']
+      @config['refresh_token'] = json['refresh_token']
+	end
 	@config['expiry'] = Time.now + json['expires_in'] - 120
 	@config['state'] = ''
 	saveConfig
